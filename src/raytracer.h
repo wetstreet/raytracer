447d23b4928c76f21f16e0889f1447a00e1bc660
wetstreet <ericchan1996@qq.com>
2022-07-27 23:44:53 +0800

    smoke & final scene

--- src/raytracer.h
+++ src/raytracer.h
@@ -8,6 +8,9 @@
 #include "material.h"
 #include "moving_sphere.h"
 #include "aarect.h"
+#include "box.h"
+#include "constant_medium.h"
+#include "bvh.h"
 
 #include "ThreadPool.h"
 
@@ -33,13 +36,13 @@ color ray_color(const ray& r, const color& background, const hittable& world, in
 }
 
 // screen
-int image_width = 600;
-int image_height = 600;
-int samples_per_pixel = 200;
+int image_width = 800;
+int image_height = 800;
+int samples_per_pixel = 10000;
 const int max_depth = 50;
 
 // camera
-point3 lookfrom(278, 278, -800);
+point3 lookfrom(478, 278, -600);
 point3 lookat(278, 278, 0);
 vec3 vup(0, 1, 0);
 float vfov = 40;
@@ -132,13 +135,41 @@ public:
 		world.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
 		world.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
 
-		image_width = 600;
-		image_height = 600;
-		samples_per_pixel = 200;
-		background = color(0, 0, 0);
-		lookfrom = point3(278, 278, -800);
-		lookat = point3(278, 278, 0);
-		vfov = 40.0;
+		shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+		box1 = make_shared<rotate_y>(box1, 15);
+		box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+		world.add(box1);
+
+		shared_ptr<hittable> box2 = make_shared<box>(point3(0, 0, 0), point3(165, 165, 165), white);
+		box2 = make_shared<rotate_y>(box2, -18);
+		box2 = make_shared<translate>(box2, vec3(130, 0, 65));
+		world.add(box2);
+	}
+
+	void init_cornell_smoke()
+	{
+		auto red = make_shared<lambertian>(color(.65, .05, .05));
+		auto white = make_shared<lambertian>(color(.73, .73, .73));
+		auto green = make_shared<lambertian>(color(.12, .45, .15));
+		auto light = make_shared<diffuse_light>(color(7, 7, 7));
+
+		world.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+		world.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+		world.add(make_shared<xz_rect>(113, 443, 127, 432, 554, light));
+		world.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+		world.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+		world.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+		shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+		box1 = make_shared<rotate_y>(box1, 15);
+		box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+
+		shared_ptr<hittable> box2 = make_shared<box>(point3(0, 0, 0), point3(165, 165, 165), white);
+		box2 = make_shared<rotate_y>(box2, -18);
+		box2 = make_shared<translate>(box2, vec3(130, 0, 65));
+
+		world.add(make_shared<constant_medium>(box1, 0.01, color(0, 0, 0)));
+		world.add(make_shared<constant_medium>(box2, 0.01, color(1, 1, 1)));
 	}
 
 	void init_random_scene()
@@ -188,13 +219,74 @@ public:
 		world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 	}
 
+	void init_final_scene()
+	{
+		hittable_list boxes1;
+		auto ground = make_shared<lambertian>(color(0.48, 0.83, 0.53));
+
+		const int boxes_per_side = 20;
+		for (int i = 0; i < boxes_per_side; i++) {
+			for (int j = 0; j < boxes_per_side; j++) {
+				auto w = 100.0;
+				auto x0 = -1000.0 + i * w;
+				auto z0 = -1000.0 + j * w;
+				auto y0 = 0.0;
+				auto x1 = x0 + w;
+				auto y1 = random_double(1, 101);
+				auto z1 = z0 + w;
+
+				boxes1.add(make_shared<box>(point3(x0, y0, z0), point3(x1, y1, z1), ground));
+			}
+		}
+
+		world.add(make_shared<bvh_node>(boxes1, 0, 1));
+
+		auto light = make_shared<diffuse_light>(color(7, 7, 7));
+		world.add(make_shared<xz_rect>(123, 423, 147, 412, 554, light));
+
+		auto center1 = point3(400, 400, 200);
+		auto center2 = center1 + vec3(30, 0, 0);
+		auto moving_sphere_material = make_shared<lambertian>(color(0.7, 0.3, 0.1));
+		world.add(make_shared<moving_sphere>(center1, center2, 0, 1, 50, moving_sphere_material));
+
+		world.add(make_shared<sphere>(point3(260, 150, 45), 50, make_shared<dielectric>(1.5)));
+		world.add(make_shared<sphere>(
+			point3(0, 150, 145), 50, make_shared<metal>(color(0.8, 0.8, 0.9), 1.0)
+			));
+
+		auto boundary = make_shared<sphere>(point3(360, 150, 145), 70, make_shared<dielectric>(1.5));
+		world.add(boundary);
+		world.add(make_shared<constant_medium>(boundary, 0.2, color(0.2, 0.4, 0.9)));
+		boundary = make_shared<sphere>(point3(0, 0, 0), 5000, make_shared<dielectric>(1.5));
+		world.add(make_shared<constant_medium>(boundary, .0001, color(1, 1, 1)));
+
+		auto emat = make_shared<lambertian>(make_shared<image_texture>("res/earthmap.jpg"));
+		world.add(make_shared<sphere>(point3(400, 200, 400), 100, emat));
+		auto pertext = make_shared<noise_texture>(0.1);
+		world.add(make_shared<sphere>(point3(220, 280, 300), 80, make_shared<lambertian>(pertext)));
+
+		hittable_list boxes2;
+		auto white = make_shared<lambertian>(color(.73, .73, .73));
+		int ns = 1000;
+		for (int j = 0; j < ns; j++) {
+			boxes2.add(make_shared<sphere>(point3::random(0, 165), 10, white));
+		}
+
+		world.add(make_shared<translate>(
+			make_shared<rotate_y>(
+				make_shared<bvh_node>(boxes2, 0.0, 1.0), 15),
+			vec3(-100, 270, 395)
+			)
+		);
+	}
+
 	void render(uint8_t* _pixels)
 	{
 		pixels = _pixels;
 		startTime = glfwGetTime();
 
 		// world
-		init_cornell_box();
+		init_final_scene();
 
 		// Camera
 		cam.init(lookfrom, lookat, vup, vfov, image_width / (float)image_height, aperture, dist_to_focus, 0.0, 1.0);
@@ -252,7 +344,7 @@ public:
 		startTime = glfwGetTime();
 
 		// world
-		init_two_spheres();
+		init_final_scene();
 
 		// Camera
 		cam.init(lookfrom, lookat, vup, vfov, image_width / (float)image_height, aperture, dist_to_focus, 0.0, 1.0);
