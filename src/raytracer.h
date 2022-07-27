ded1f8f19f3119e2cc25347edab5489a4641fe37
wetstreet <ericchan1996@qq.com>
2022-07-27 18:54:58 +0800

    rect light & cornell box

--- src/raytracer.h
+++ src/raytracer.h
@@ -7,44 +7,48 @@
 #include "camera.h"
 #include "material.h"
 #include "moving_sphere.h"
+#include "aarect.h"
 
 #include "ThreadPool.h"
 
-color ray_color(const ray& r, const hittable& world, int depth) {
+color ray_color(const ray& r, const color& background, const hittable& world, int depth) {
 	hit_record rec;
 
 	// If we've exceeded the ray bounce limit, no more light is gathered.
 	if (depth <= 0)
 		return color(0, 0, 0);
 
-	if (world.hit(r, 0.001, infinity, rec)) {
-		ray scattered;
-		color attenuation;
-		if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-			return attenuation * ray_color(scattered, world, depth - 1);
-		return color(0, 0, 0);
-	}
-	vec3 unit_direction = unit_vector(r.direction());
-	auto t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+	// If the ray hits nothing, return the background color.
+	if (!world.hit(r, 0.001, infinity, rec))
+		return background;
+
+	ray scattered;
+	color attenuation;
+	color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+
+	if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+		return emitted;
+
+	return emitted + attenuation * ray_color(scattered, background, world, depth - 1);
 }
 
 // screen
-int image_width = 400;
-int image_height = 225;
-int samples_per_pixel = 50;
+int image_width = 600;
+int image_height = 600;
+int samples_per_pixel = 200;
 const int max_depth = 50;
 
 // camera
-point3 lookfrom(13, 2, 3);
-point3 lookat(0, 0, 0);
+point3 lookfrom(278, 278, -800);
+point3 lookat(278, 278, 0);
 vec3 vup(0, 1, 0);
-float vfov = 20;
+float vfov = 40;
 float dist_to_focus = 10.0f;
 float aperture = 0.0f;
+color background(0, 0, 0);
 
 // multi-threading
-ThreadPool pool(std::thread::hardware_concurrency());
+ThreadPool pool(std::thread::hardware_concurrency() - 1);
 std::mutex tile_mutex;
 int finishedTileCount = 0;
 int totalTileCount = 0;
@@ -102,6 +106,41 @@ public:
 		world.add(globe);
 	}
 
+	void init_simple_light()
+	{
+		auto pertext = make_shared<noise_texture>(4);
+		world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
+		world.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
+
+		auto difflight = make_shared<diffuse_light>(color(4, 4, 4));
+		world.add(make_shared<xy_rect>(3, 5, 1, 3, -2, difflight));
+
+		background = color(0, 0, 0);
+	}
+
+	void init_cornell_box()
+	{
+		auto red = make_shared<lambertian>(color(.65, .05, .05));
+		auto white = make_shared<lambertian>(color(.73, .73, .73));
+		auto green = make_shared<lambertian>(color(.12, .45, .15));
+		auto light = make_shared<diffuse_light>(color(15, 15, 15));
+
+		world.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+		world.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+		world.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+		world.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+		world.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+		world.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+		image_width = 600;
+		image_height = 600;
+		samples_per_pixel = 200;
+		background = color(0, 0, 0);
+		lookfrom = point3(278, 278, -800);
+		lookat = point3(278, 278, 0);
+		vfov = 40.0;
+	}
+
 	void init_random_scene()
 	{
 		auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
@@ -155,7 +194,7 @@ public:
 		startTime = glfwGetTime();
 
 		// world
-		init_earth();
+		init_cornell_box();
 
 		// Camera
 		cam.init(lookfrom, lookat, vup, vfov, image_width / (float)image_height, aperture, dist_to_focus, 0.0, 1.0);
@@ -182,7 +221,7 @@ public:
 						auto u = (i + random_double()) / (image_width - 1);
 						auto v = (j + random_double()) / (image_height - 1);
 						ray r = cam.get_ray(u, v);
-						pixel_color += ray_color(r, world, max_depth);
+						pixel_color += ray_color(r, background, world, max_depth);
 					}
 
 					write_color(pixel_color, i, j);
@@ -227,7 +266,7 @@ public:
 					auto u = (i + random_double()) / (image_width - 1);
 					auto v = (j + random_double()) / (image_height - 1);
 					ray r = cam.get_ray(u, v);
-					pixel_color += ray_color(r, world, max_depth);
+					pixel_color += ray_color(r, background, world, max_depth);
 				}
 
 				write_color(pixel_color, i, j);
